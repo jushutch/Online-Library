3b74d993f840c4a55e51f207b8438027ebc42316
jushutch <jushutch@umich.edu>
2020-07-10 02:08:22 -0400

    Changed From POST to GET
    Using GET for certain APIs for readability.
    Will revisit for security concerns.

--- API/BookAPI.php
+++ API/BookAPI.php
@@ -1,8 +1,6 @@
 <?php
     require_once __DIR__ . "/BookSearchService.php";
-    $postdata = file_get_contents("php://input");
-    $request = json_decode($postdata, true);
-    $searchString = $request['searchText'];
+    $searchString = $_GET['searchText'];
     $bookSearchService = new BookSearchService();
     $searchResults = $bookSearchService->searchForBook($searchString);
     echo json_encode($searchResults);
--- API/ISBNApi.php
+++ API/ISBNApi.php
@@ -1,8 +1,6 @@
 <?php
     require_once __DIR__ . "/ISBNService.php";
-    $postdata = file_get_contents("php://input");
-    $request = json_decode($postdata, true);
-    $isbn = $request['isbn'];
+    $isbn = $_GET['isbn'];
     $ISBNService = new ISBNService();
     $book = $ISBNService->getBookFromAPIByISBN($isbn);
     echo json_encode($book);
