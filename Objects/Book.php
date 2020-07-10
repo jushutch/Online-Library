a017be168dbb1a8830e39c57b385648e0a3bba58
jushutch <jushutch@umich.edu>
2020-07-10 02:19:44 -0400

    Refactor and Db Functions
    Completely redesigned Book object. Functions
    as a data structure with valid state. Added
    new properties. Implemented functions to
    query database for new functionality.

--- Objects/Book.php
+++ Objects/Book.php
@@ -1,32 +1,91 @@
 <?php
 
-require_once "database/BookDbGateway.php";
+require_once __DIR__ . "/../database/BookDbGateway.php";
 
 class Book {
+    public $bookId;
     public $title;
+    public $subtitle;
     public $authors;
+    public $publisher;
     public $year;
     public $genre;
+    public $pageCount;
     public $isbn;
     public $series;
     public $seriesNumber;
+    public $description;
+    public $imageURL;
+    public $isAvailable;
 
-    private $bookDbGateway;
-
-    public function __construct() {
-        $this->bookDbGateway = new BookDbGateway();
+    public function __construct($title,
+                                $authors,
+                                $publisher,
+                                $year,
+                                $genre,
+                                $pageCount,
+                                $isbn,
+                                $description,
+                                $isAvailable,
+                                $imageURL = null,
+                                $series = null,
+                                $seriesNumber = null,
+                                $subtitle = null,
+                                $bookId = null) {
+        $this->title = $title;
+        $this->authors = explode(",", $authors);
+        $this->publisher = $publisher;
+        $this->year = $year;
+        $this->genre = $genre;
+        $this->pageCount = $pageCount;
+        $this->isbn = $isbn;
+        $this->description = $description;
+        $this->isAvailable = $isAvailable;
+        $this->imageURL = $imageURL ?? "images/logo.png";
+        $this->series = $series;
+        $this->seriesNumber = $seriesNumber;
+        $this->subtitle = $subtitle;
+        $this->bookId = $bookId;
     }
 
-    public function createNewBook() {
-        $this->title = $_POST['title'];
-        $this->authors = explode(",", $_POST['authors']);
-        $this->year = (int)$_POST['year'];
-        $this->genre = $_POST['genre'];
-        $this->isbn = $_POST['isbn'];
-        $this->series = $_POST['series'] ? $_POST['series'] : null;
-        $this->seriesNumber = $_POST['seriesNumber'] ? $_POST['seriesNumber'] : null;
-        $_POST = array();
-        return $this->bookDbGateway->insertBook($this);
+    static public function fromArray($book) {
+        return new self(
+            $book['title'],
+            $book['authors'] ?? $book['author_list'],
+            $book['publisher'],
+            $book['year'],
+            $book['genre'],
+            $book['pageCount'] ?? $book['page_count'],
+            $book['isbn'],
+            $book['description'],
+            $book['isAvailable'] ?? $book['is_available'],
+            isset($book['imageURL']) ? $book['imageURL'] :
+                isset($book['image_url']) ? $book['image_url'] : null,
+            isset($book['series']) && $book['series'] ? $book['series'] : null,
+            isset($book['seriesNumber']) ? $book['seriesNumber'] :
+                isset($book['series_number']) ? $book['series_number'] : null,
+            isset($book['subtitle']) && $book['subtitle'] ? $book['subtitle'] : null,
+            isset($book['bookId']) ? $book['bookId'] :
+                isset($book['book_id']) ? $book['book_id'] : null
+        );
     }
 
+    public function jsonSerialize() {
+        return [
+            'bookId' => $this->bookId,
+            'title' => $this->title,
+            'authors' => $this->authors,
+            'publisher' => $this->publisher,
+            'year' => $this->year,
+            'genre' => $this->genre,
+            'pageCount' => $this->pageCount,
+            'isbn' => $this->isbn,
+            'description' => $this->description,
+            'isAvailable' => $this->isAvailable,
+            'imageURL' => $this->imageURL,
+            'series' => $this->series,
+            'seriesNumber' => $this->seriesNumber,
+            'subtitle' => $this->subtitle
+        ];
+    }
 }
--- database/BookDbGateway.php
+++ database/BookDbGateway.php
@@ -38,6 +38,15 @@ class BookDbGateway extends Gateway {
         $stmt->close();
     }
 
+    public function deleteAuthors($bookId) {
+        $sql = "DELETE FROM author
+                WHERE author.book_id = ?";
+        $stmt = $this->conn->prepare($sql);
+        $stmt->bind_param("i", $bookId);
+        $stmt->execute();
+        $stmt->close();
+    }
+
     public function insertDescription($description, $bookId) {
         $sql = "INSERT INTO book_description(book_id, description) 
                     VALUES ( ? , ? )";
@@ -69,8 +78,17 @@ class BookDbGateway extends Gateway {
         $stmt->close();
     }
 
+    public function checkInBook($bookId) {
+        $sql = "UPDATE checkout SET return_date = CURRENT_TIMESTAMP() WHERE checkout.book_id = ?";
+        $stmt = $this->conn->prepare($sql);
+        $stmt->bind_param("i", $bookId);
+        $stmt->execute();
+        $stmt->close();
+        echo "end of checkInBook reached";
+    }
+
     public function isBookIdCheckedOut($bookId) {
-        $sql = "SELECT checkout_id FROM checkout WHERE book_id = ?";
+        $sql = "SELECT checkout_id FROM checkout WHERE book_id = ? AND return_date IS NULL";
         $stmt = $this->conn->prepare($sql);
         $stmt->bind_param("i", $bookId);
         $stmt->execute();
@@ -78,6 +96,31 @@ class BookDbGateway extends Gateway {
         $rowCount = $stmt->num_rows;
         $stmt->close();
         return $rowCount > 0;
+    }
+
+    public function getCheckedOutBooksForAccount($accountNumber) {
+        $selectAuthors = "(SELECT GROUP_CONCAT(author_name) as author_list, book_id 
+                            FROM author
+                            GROUP BY book_id) as authors";
+        $sql = "SELECT DISTINCT checkout.due_date, author_list, book.book_id, title, subtitle, publisher, year, page_count, isbn, series, series_number, description, image_url, genre
+                FROM $selectAuthors
+                LEFT JOIN book on book.book_id = authors.book_id
+                left JOIN book_description on book_description.book_id = book.book_id
+                left JOIN book_image on book_image.book_id = book.book_id
+                left join author on author.book_id = book.book_id
+                INNER JOIN genre on genre.genre_id = book.genre_id
+                left join checkout on checkout.book_id = book.book_id
+                WHERE checkout.account_number = ? AND checkout.return_date IS NULL";
+        $stmt = $this->conn->prepare($sql);
+        $stmt->bind_param("i", $accountNumber);
+        $stmt->execute();
+        $result = $stmt->get_result();
+        $bookData = [];
+        while($book = $result->fetch_assoc()) {
+            $bookData[] = $book;
+        }
+        $stmt->close();
+        return $bookData;
     }
 
     public function getBookIdByISBN($isbn) {
@@ -112,11 +155,49 @@ class BookDbGateway extends Gateway {
         return $book;
     }
 
+    public function updateBook(array $book) {
+        $sql = "UPDATE book, book_description, book_image, genre
+                SET title = ?, subtitle = ?, publisher = ?, year = ?, page_count = ?, isbn = ?, series = ?, series_number = ?, description = ?, image_url = ?, genre = ?
+                WHERE book.book_id = book_description.book_id
+                AND book.book_id = book_image.book_id
+                AND book.genre_id = genre.genre_id
+                AND book.book_id = ?";
+        $stmt = $this->conn->prepare($sql);
+        $stmt->bind_param("sssiississsi",
+            $book['title'],
+            $book['subtitle'],
+            $book['publisher'],
+            $book['year'],
+            $book['pageCount'],
+            $book['isbn'],
+            $book['series'],
+            $book['seriesNumber'],
+            $book['description'],
+            $book['imageURL'],
+            $book['genre'],
+            $book['bookId']
+        );
+        $stmt->execute();
+        var_dump($book);
+        $stmt->close();
+        $this->deleteAuthors($book['bookId']);
+        $this->insertAuthors($book['authors'], $book['bookId']);
+    }
+
     public function deleteBookByISBN($isbn) {
         $sql = "DELETE FROM book
                 WHERE isbn = ?";
         $stmt = $this->conn->prepare($sql);
         $stmt->bind_param("s", $isbn);
+        $stmt->execute();
+        $stmt->close();
+    }
+
+    public function deleteBookByBookId($bookId) {
+        $sql = "DELETE FROM book
+                WHERE book_id = ?";
+        $stmt = $this->conn->prepare($sql);
+        $stmt->bind_param("s", $bookId);
         $stmt->execute();
         $stmt->close();
     }
